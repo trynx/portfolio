8825dfb047e790e9c560ce4b7840afbe1a6ccfbc
Nicolas Caro <trynx93@gmail.,com>
2022-07-03 23:07:58 +0300

    Try netlify deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+    // Target must be serverless
+    target: "serverless",
 };
 
 module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+const myKey = process.env.NEXT_PUBLIC_KEY;
+
 export default function Home() {
     return (
         <div className={styles.container}>
